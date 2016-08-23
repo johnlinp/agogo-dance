c8cd5d3140cae8aa295fef3df9dd08948dc95f5a
johnlinp <johnlinp@gmail.com>
2016-08-24 01:50:09 +0800

    Add photos

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+var photoPageIdx = 0;
+
 (function() {
     var putEnvelopeName = function() {
         var name = window.location.hash.substr(1);
@@ -19,21 +21,56 @@
             'thanks',
         ];
 
+        var createPhoto = function() {
+            var photoIdx = photoPageIdx % 30;
+            var photoId = 'photo-' + photoPageIdx;
+            var container = document.getElementById('container');
+            var photo = document.createElement('div');
+            var inner = document.createElement('div');
+            var img = document.createElement('img');
+
+            img.setAttribute('src', 'photo/' + photoIdx + '.jpg');
+            inner.classList.add('vertical-center-inner');
+            photo.setAttribute('id', photoId);
+            photo.classList.add('full');
+            photo.classList.add('hidden');
+            photo.classList.add('vertical-center-outer');
+
+            inner.appendChild(img);
+            photo.appendChild(inner);
+            container.appendChild(photo);
+
+            ++photoPageIdx;
+
+            return photoId;
+        };
+
         var setTransition = function(fromIdx, toIdx) {
             var from = document.getElementById(items[fromIdx]);
             var to = document.getElementById(items[toIdx]);
 
             from.addEventListener('click', function() {
+                if (from.classList.contains('front-animation')) {
+                    return;
+                }
+
                 from.classList.add('front-animation');
                 to.classList.add('back-animation');
 
-                if (toIdx < items.length - 1) {
-                    to.addEventListener('animationend', function() {
-                        to.style.opacity = 1;
-                        to.classList.remove('back-animation');
-                        setTransition(fromIdx + 1, toIdx + 1);
-                    });
-                }
+                to.addEventListener('animationend', function() {
+                    from.style.visibility = 'hidden';
+                    to.style.visibility = 'visible';
+
+                    from.classList.remove('front-animation');
+                    to.classList.remove('back-animation');
+
+                    if (toIdx == items.length - 1) {
+                        var photoId = createPhoto();
+                        items.push(photoId);
+                    }
+
+                    setTransition(toIdx, toIdx + 1);
+                });
             });
         };
 
